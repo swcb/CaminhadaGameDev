b92d57100c047cb448f6f0c20140189c119d775a
Silvio Willian Caires Batista <201700017662@office.ufs.br>
2019-07-26 08:28:25 -0300

    Adicionando projeto Acqua Scape

---
+++ Acqua Scape/Assets/Script/Gota.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gota : MonoBehaviour
+{
+
+    private     Rigidbody2D     gotaRb;
+    private     float           atrito;
+    public      float           atritoMax;
+    public      Vector3         posicao;
+    public      GameObject      gotaPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        gotaRb = GetComponent<Rigidbody2D>();
+        gotaRb.gravityScale = 15;
+        atrito = Random.Range(3, atritoMax);
+        gotaRb.drag = atrito;
+
+        posicao = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBecameInvisible() {
+        Instantiate(gotaPrefab, posicao, transform.localRotation);
+        Destroy(this.gameObject);
+        pontuacao.pontos += 1;
+    }
+}
---
+++ Acqua Scape/Assets/Script/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+        //Variaveis globais
+    private Rigidbody2D     playerRb;
+    private SpriteRenderer  playerSprite;
+    public  float           velocidade;
+    public  bool            flipX;
+    
+        // Start is called before the first frame update
+    void Start(){
+        
+        playerRb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+
+    }
+
+        // Update is called once per frame
+    void Update(){
+
+        if(Input.GetMouseButtonDown(0)){
+            velocidade *= -1;
+            flipX = !flipX;
+            playerSprite.flipX = flipX;
+        }
+
+        playerRb.velocity = new Vector2(velocidade, playerRb.velocity.y); //ou (velocidade, playerRb.velocity.y)
+
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao) {
+        if(colisao.gameObject.tag == "Parede") {
+            SceneManager.LoadScene("morreu");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D colisao) {
+        if(colisao.gameObject.tag == "Gota") {
+            SceneManager.LoadScene("morreu");
+        }
+    }
+
+}

c33e80f8d4dcaccec92f425fc0748e620aaf281a
Silvio Willian Caires Batista <201700017662@office.ufs.br>
2019-07-29 11:59:09 -0300

    Movimentação do personagem e animação na gota

--- Acqua Scape/Assets/Script/Player.cs
+++ Acqua Scape/Assets/Script/Player.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Player : MonoBehaviour
 {
         //Variaveis globais
     private Rigidbody2D     playerRb;
     private SpriteRenderer  playerSprite;
-    public  float           velocidade;
+    public  float           velocidadeMax;
+    public  float           velocidadeMin;
+    public float           velocidadeAtual;
+    public float           aceleracao;
     public  bool            flipX;
     
         // Start is called before the first frame update
@@ -16,19 +20,46 @@ public class Player : MonoBehaviour
         
         playerRb = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
-
+        velocidadeAtual = velocidadeMax;
     }
 
         // Update is called once per frame
     void Update(){
+        
 
-        if(Input.GetMouseButtonDown(0)){
-            velocidade *= -1;
+        /*
+            if(Input.GetMouseButtonDown(0)){
+                velocidade *= -1;
+                flipX = !flipX;
+                playerSprite.flipX = flipX;
+            }
+        
+        float movimento = Input.GetAxis("Horizontal");
+        if(movimento > 0){
+            velocidadeAtual = (Math.Abs(velocidadeAtual) + velocidadeMin) * movimento * aceleracao;
+            if(velocidadeAtual > velocidadeMax) velocidadeAtual = velocidadeMax;
+        }
+        else if(movimento < 0){
+            velocidadeAtual = (Math.Abs(velocidadeAtual) + velocidadeMin) * movimento * aceleracao;
+            if(velocidadeAtual < (-1 * velocidadeMax)) velocidadeAtual = -1 * velocidadeMax;
+        }
+        else 
+            velocidadeAtual = 0;   
+
+        */
+        if(Math.Abs(velocidadeAtual) < velocidadeMax){
+            velocidadeAtual *= aceleracao;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //velocidadeAtual = -1 * (velocidadeMin * 1/velocidadeAtual + velocidadeMin);
+            if (velocidadeAtual > 0) velocidadeAtual = velocidadeMin * -1;
+            else if (velocidadeAtual < 0) velocidadeAtual = velocidadeMin;
             flipX = !flipX;
             playerSprite.flipX = flipX;
         }
 
-        playerRb.velocity = new Vector2(velocidade, playerRb.velocity.y); //ou (velocidade, playerRb.velocity.y)
+        playerRb.velocity = new Vector2(velocidadeAtual, playerRb.velocity.y); //ou (velocidade, playerRb.velocity.y)
 
     }
 
--- Acqua Scape/Assets/Script/iniciarJogo.cs
+++ Acqua Scape/Assets/Script/iniciarJogo.cs
@@ -14,7 +14,7 @@ public class iniciarJogo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetKeyDown(KeyCode.Space)){
             SceneManager.LoadScene("fase");
         }
     }
